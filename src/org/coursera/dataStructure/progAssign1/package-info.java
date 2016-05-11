1507bcc2ed177b46bf5e7a36e10eb5087cc4a896
pranjal1988 <pranjal1988@gmail.com>
2016-05-12 01:44:12 +0530

    defined the package and package doc for programming assignment 1 of DS Course on coursera

---
+++ src/org/coursera/dataStructure/progAssign1/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package is for the programming Assignment 1 questions for the 
+ * course Data Structure on Coursera offered by 
+ * University of California, San Diego & Higher School of Economic.
+ */
+package org.coursera.dataStructure.progAssign1;
